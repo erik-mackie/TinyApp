f536fb1a67291a2f997d0a36cb75b1ff3a9cb111
erik-mackie <erik.mackie@gmail.com>
2018-09-05 02:33:50 +0000

    added second route

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -13,10 +15,21 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+app.get("/urls/:id", (req, res) => {
+
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+   };
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
