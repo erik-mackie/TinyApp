afa3f06210b47e1cdd7d5c71a24d90e616836cad
erik-mackie <erik.mackie@gmail.com>
2018-09-05 02:21:14 +0000

    server sending html

--- express_server.js
+++ express_server.js
@@ -1,14 +1,8 @@
 "use strict"
 
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
-const randomNumber = require("./randomNumber");
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set("view engine", "ejs");
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,45 +13,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/u/:shortURL", (req, res) => {
-   let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-app.get("/urls/:id", (req, res) => {
-
-  let templateVars = {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
-   };
-
-  res.render("urls_show", templateVars);
-});
-
-
-app.post("/urls", (req, res) => {
-  const randomKey= randomNumber();
-  console.log(req.body);  // debug statement to see POST parameters
-  urlDatabase[`${randomKey}`] = req.body["longURL"];
-  console.log(urlDatabase)
-  let templateVars = { urls: urlDatabase };
-  res.redirect(`urls/${randomKey}`);
-
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
-//Respond with a redirection to http://localhost:8080/urls/<shortURL>,
-// where <shortURL> indicates the random string you generated to represent the original URL.
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
